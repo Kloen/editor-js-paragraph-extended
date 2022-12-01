12dae2ccac07e8bd950b77e01778c9a2186b5e77
Kloen Lansfiel <kloen@kloenlansfiel.com>
2022-12-01 21:01:51 +0100

    chore(reformat): reformat src.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
  */
 import './index.css';
 
-import { IconText } from '@codexteam/icons'
+import {IconText} from '@codexteam/icons'
 
 /**
  * Base Paragraph Block for the Editor.js.
@@ -26,230 +26,230 @@ import { IconText } from '@codexteam/icons'
  * @property {String} text — Paragraph's content. Can include HTML tags: <a><b><i>
  */
 export default class Paragraph {
-  /**
-   * Default placeholder for Paragraph Tool
-   *
-   * @return {string}
-   * @constructor
-   */
-  static get DEFAULT_PLACEHOLDER() {
-    return '';
-  }
-
-  /**
-   * Render plugin`s main Element and fill it with saved data
-   *
-   * @param {object} params - constructor params
-   * @param {ParagraphData} params.data - previously saved data
-   * @param {ParagraphConfig} params.config - user config for Tool
-   * @param {object} params.api - editor.js api
-   * @param {boolean} readOnly - read only mode flag
-   */
-  constructor({data, config, api, readOnly}) {
-    this.api = api;
-    this.readOnly = readOnly;
-
-    this._CSS = {
-      block: this.api.styles.block,
-      wrapper: 'ce-paragraph'
-    };
-
-    if (!this.readOnly) {
-      this.onKeyUp = this.onKeyUp.bind(this);
+    /**
+     * Default placeholder for Paragraph Tool
+     *
+     * @return {string}
+     * @constructor
+     */
+    static get DEFAULT_PLACEHOLDER() {
+        return '';
     }
 
     /**
-     * Placeholder for paragraph if it is first Block
-     * @type {string}
+     * Render plugin`s main Element and fill it with saved data
+     *
+     * @param {object} params - constructor params
+     * @param {ParagraphData} params.data - previously saved data
+     * @param {ParagraphConfig} params.config - user config for Tool
+     * @param {object} params.api - editor.js api
+     * @param {boolean} readOnly - read only mode flag
      */
-    this._placeholder = config.placeholder ? config.placeholder : Paragraph.DEFAULT_PLACEHOLDER;
-    this._data = {};
-    this._element = this.drawView();
-    this._preserveBlank = config.preserveBlank !== undefined ? config.preserveBlank : false;
+    constructor({data, config, api, readOnly}) {
+        this.api = api;
+        this.readOnly = readOnly;
 
-    this.data = data;
-  }
+        this._CSS = {
+            block: this.api.styles.block,
+            wrapper: 'ce-paragraph'
+        };
 
-  /**
-   * Check if text content is empty and set empty string to inner html.
-   * We need this because some browsers (e.g. Safari) insert <br> into empty contenteditanle elements
-   *
-   * @param {KeyboardEvent} e - key up event
-   */
-  onKeyUp(e) {
-    if (e.code !== 'Backspace' && e.code !== 'Delete') {
-      return;
+        if (!this.readOnly) {
+            this.onKeyUp = this.onKeyUp.bind(this);
+        }
+
+        /**
+         * Placeholder for paragraph if it is first Block
+         * @type {string}
+         */
+        this._placeholder = config.placeholder ? config.placeholder : Paragraph.DEFAULT_PLACEHOLDER;
+        this._data = {};
+        this._element = this.drawView();
+        this._preserveBlank = config.preserveBlank !== undefined ? config.preserveBlank : false;
+
+        this.data = data;
     }
 
-    const {textContent} = this._element;
+    /**
+     * Check if text content is empty and set empty string to inner html.
+     * We need this because some browsers (e.g. Safari) insert <br> into empty contenteditanle elements
+     *
+     * @param {KeyboardEvent} e - key up event
+     */
+    onKeyUp(e) {
+        if (e.code !== 'Backspace' && e.code !== 'Delete') {
+            return;
+        }
 
-    if (textContent === '') {
-      this._element.innerHTML = '';
-    }
-  }
+        const {textContent} = this._element;
 
-  /**
-   * Create Tool's view
-   * @return {HTMLElement}
-   * @private
-   */
-  drawView() {
-    let div = document.createElement('DIV');
-
-    div.classList.add(this._CSS.wrapper, this._CSS.block);
-    div.contentEditable = false;
-    div.dataset.placeholder = this.api.i18n.t(this._placeholder);
-
-    if (!this.readOnly) {
-      div.contentEditable = true;
-      div.addEventListener('keyup', this.onKeyUp);
+        if (textContent === '') {
+            this._element.innerHTML = '';
+        }
     }
 
-    return div;
-  }
+    /**
+     * Create Tool's view
+     * @return {HTMLElement}
+     * @private
+     */
+    drawView() {
+        let div = document.createElement('DIV');
 
-  /**
-   * Return Tool's view
-   *
-   * @returns {HTMLDivElement}
-   */
-  render() {
-    return this._element;
-  }
+        div.classList.add(this._CSS.wrapper, this._CSS.block);
+        div.contentEditable = false;
+        div.dataset.placeholder = this.api.i18n.t(this._placeholder);
 
-  /**
-   * Method that specified how to merge two Text blocks.
-   * Called by Editor.js by backspace at the beginning of the Block
-   * @param {ParagraphData} data
-   * @public
-   */
-  merge(data) {
-    let newData = {
-      text : this.data.text + data.text
-    };
+        if (!this.readOnly) {
+            div.contentEditable = true;
+            div.addEventListener('keyup', this.onKeyUp);
+        }
 
-    this.data = newData;
-  }
-
-  /**
-   * Validate Paragraph block data:
-   * - check for emptiness
-   *
-   * @param {ParagraphData} savedData — data received after saving
-   * @returns {boolean} false if saved data is not correct, otherwise true
-   * @public
-   */
-  validate(savedData) {
-    if (savedData.text.trim() === '' && !this._preserveBlank) {
-      return false;
+        return div;
     }
 
-    return true;
-  }
+    /**
+     * Return Tool's view
+     *
+     * @returns {HTMLDivElement}
+     */
+    render() {
+        return this._element;
+    }
 
-  /**
-   * Extract Tool's data from the view
-   * @param {HTMLDivElement} toolsContent - Paragraph tools rendered view
-   * @returns {ParagraphData} - saved data
-   * @public
-   */
-  save(toolsContent) {
-    return {
-      text: toolsContent.innerHTML
-    };
-  }
+    /**
+     * Method that specified how to merge two Text blocks.
+     * Called by Editor.js by backspace at the beginning of the Block
+     * @param {ParagraphData} data
+     * @public
+     */
+    merge(data) {
+        let newData = {
+            text: this.data.text + data.text
+        };
 
-  /**
-   * On paste callback fired from Editor.
-   *
-   * @param {PasteEvent} event - event with pasted data
-   */
-  onPaste(event) {
-    const data = {
-      text: event.detail.data.innerHTML
-    };
+        this.data = newData;
+    }
 
-    this.data = data;
-  }
+    /**
+     * Validate Paragraph block data:
+     * - check for emptiness
+     *
+     * @param {ParagraphData} savedData — data received after saving
+     * @returns {boolean} false if saved data is not correct, otherwise true
+     * @public
+     */
+    validate(savedData) {
+        if (savedData.text.trim() === '' && !this._preserveBlank) {
+            return false;
+        }
 
-  /**
-   * Enable Conversion Toolbar. Paragraph can be converted to/from other tools
-   */
-  static get conversionConfig() {
-    return {
-      export: 'text', // to convert Paragraph to other block, use 'text' property of saved data
-      import: 'text' // to covert other block's exported string to Paragraph, fill 'text' property of tool data
-    };
-  }
+        return true;
+    }
 
-  /**
-   * Sanitizer rules
-   */
-  static get sanitize() {
-    return {
-      text: {
-        br: true,
-      }
-    };
-  }
+    /**
+     * Extract Tool's data from the view
+     * @param {HTMLDivElement} toolsContent - Paragraph tools rendered view
+     * @returns {ParagraphData} - saved data
+     * @public
+     */
+    save(toolsContent) {
+        return {
+            text: toolsContent.innerHTML
+        };
+    }
 
-  /**
-   * Returns true to notify the core that read-only mode is supported
-   *
-   * @return {boolean}
-   */
-  static get isReadOnlySupported() {
-    return true;
-  }
+    /**
+     * On paste callback fired from Editor.
+     *
+     * @param {PasteEvent} event - event with pasted data
+     */
+    onPaste(event) {
+        const data = {
+            text: event.detail.data.innerHTML
+        };
 
-  /**
-   * Get current Tools`s data
-   * @returns {ParagraphData} Current data
-   * @private
-   */
-  get data() {
-    let text = this._element.innerHTML;
+        this.data = data;
+    }
 
-    this._data.text = text;
+    /**
+     * Enable Conversion Toolbar. Paragraph can be converted to/from other tools
+     */
+    static get conversionConfig() {
+        return {
+            export: 'text', // to convert Paragraph to other block, use 'text' property of saved data
+            import: 'text' // to covert other block's exported string to Paragraph, fill 'text' property of tool data
+        };
+    }
 
-    return this._data;
-  }
+    /**
+     * Sanitizer rules
+     */
+    static get sanitize() {
+        return {
+            text: {
+                br: true,
+            }
+        };
+    }
 
-  /**
-   * Store data in plugin:
-   * - at the this._data property
-   * - at the HTML
-   *
-   * @param {ParagraphData} data — data to set
-   * @private
-   */
-  set data(data) {
-    this._data = data || {};
+    /**
+     * Returns true to notify the core that read-only mode is supported
+     *
+     * @return {boolean}
+     */
+    static get isReadOnlySupported() {
+        return true;
+    }
 
-    this._element.innerHTML = this._data.text || '';
-  }
+    /**
+     * Get current Tools`s data
+     * @returns {ParagraphData} Current data
+     * @private
+     */
+    get data() {
+        let text = this._element.innerHTML;
 
-  /**
-   * Used by Editor paste handling API.
-   * Provides configuration to handle P tags.
-   *
-   * @returns {{tags: string[]}}
-   */
-  static get pasteConfig() {
-    return {
-      tags: [ 'P' ]
-    };
-  }
+        this._data.text = text;
 
-  /**
-   * Icon and title for displaying at the Toolbox
-   *
-   * @return {{icon: string, title: string}}
-   */
-  static get toolbox() {
-    return {
-      icon: IconText,
-      title: 'Text'
-    };
-  }
+        return this._data;
+    }
+
+    /**
+     * Store data in plugin:
+     * - at the this._data property
+     * - at the HTML
+     *
+     * @param {ParagraphData} data — data to set
+     * @private
+     */
+    set data(data) {
+        this._data = data || {};
+
+        this._element.innerHTML = this._data.text || '';
+    }
+
+    /**
+     * Used by Editor paste handling API.
+     * Provides configuration to handle P tags.
+     *
+     * @returns {{tags: string[]}}
+     */
+    static get pasteConfig() {
+        return {
+            tags: ['P']
+        };
+    }
+
+    /**
+     * Icon and title for displaying at the Toolbox
+     *
+     * @return {{icon: string, title: string}}
+     */
+    static get toolbox() {
+        return {
+            icon: IconText,
+            title: 'Text'
+        };
+    }
 }
